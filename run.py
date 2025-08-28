a1623519303caf590f538b0ef350e9eb8c464ccf
Vishal Maurya <vishal6361@outlook.com>
2025-08-29 00:44:02 +0530

    revert

--- run.py
+++ run.py
@@ -87,7 +87,7 @@ def main():
         subprocess.run([
             python_exe, "-m", "uvicorn", 
             "main:app", 
-            "--host", "127.0.0.1", 
+            "--host", "0.0.0.0", 
             "--port", "8000",
             "--reload"
         ])
